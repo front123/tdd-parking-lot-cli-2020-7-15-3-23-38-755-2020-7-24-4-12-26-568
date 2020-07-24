f1a3f7ec4e509b7107bc07a5f24dcd28e7aff24f
LIANGFR2 <372334264@qq.com>
2020-07-24 16:53:18 +0800

    [ParkingBoy][Front] test:should_return_correspond_car_when_parking_given_parking_lot_with_2_cars_and_parking_boy_and_a_ticket

--- src/main/java/parking/ParkingBoy.java
+++ src/main/java/parking/ParkingBoy.java
@@ -1,6 +1,18 @@
 package parking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingBoy {
+
+    private final List<Car> parkingLot;
+
+    public ParkingBoy() {
+        parkingLot = new ArrayList<>();
+    }
+    public ParkingBoy(List<Car> parkingLot) {
+        this.parkingLot = parkingLot;
+    }
 
     public String parking(Car car){
         return "1";
--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -2,8 +2,12 @@ package com.oocl.cultivation.test;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import parking.Car;
 import parking.ParkingBoy;
+
+import java.util.LinkedList;
+import java.util.List;
 
 class ParkingBoyTest {
     @Test
@@ -30,5 +34,24 @@ class ParkingBoyTest {
 
         //then
         Assertions.assertEquals(1,car.getId());
+    }
+
+    @Test
+    void should_return_correspond_car_when_parking_given_parking_lot_with_2_cars_and_parking_boy_and_a_ticket() {
+        //given
+        Car car1 = new Car(1);
+        Car car2 = new Car(2);
+        List<Car> parkingLot = new LinkedList<>();
+        parkingLot.add(car1);
+        parkingLot.add(car2);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        String ticket = "2";
+
+        //when
+        Car correspondCar = parkingBoy.fetchCar(ticket);
+
+        //then
+        Assertions.assertEquals(car2, correspondCar);
+
     }
 }
