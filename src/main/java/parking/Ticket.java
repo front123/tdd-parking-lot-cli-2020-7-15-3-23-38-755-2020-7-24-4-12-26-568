4069cca81d2f6d068be160de94b063cddcfbeaf5
LIANGFR2 <372334264@qq.com>
2020-07-24 23:20:40 +0800

    [ParkingBoy][Front] refactor: add attribute(isValid) in Ticket to indicate if ticket valid

--- src/main/java/parking/Ticket.java
+++ src/main/java/parking/Ticket.java
@@ -1,13 +1,23 @@
 package parking;
 
 public class Ticket {
-    private int id;
+    private final int id;
+    private boolean isValid;
 
     public Ticket(int id) {
         this.id = id;
+        this.isValid = true;
     }
 
     public int getId() {
         return id;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public void setValid(boolean valid) {
+        isValid = valid;
     }
 }
