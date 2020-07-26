5b8c47e9afeb331fbbe2c655c6c77f9a7a877a12
LIANGFR2 <372334264@qq.com>
2020-07-26 20:28:02 +0800

    [ParkingBoy][Front] refactor: add ParkingLots list to ParkingBoy

--- src/main/java/parking/ParkingBoy.java
+++ src/main/java/parking/ParkingBoy.java
@@ -1,34 +1,50 @@
 package parking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ParkingBoy {
 
     private final ParkingLot parkingLot;
     private final ParkingLot parkingLot2;
+    private final List<ParkingLot> parkingLots = new ArrayList<>();
 
-    public ParkingBoy(ParkingLot parkingLot) {
-        this.parkingLot = parkingLot;
-        this.parkingLot2 = null;
-    }
 
     public ParkingBoy(ParkingLot...parkingLots) {
+        ParkingLot parkingLot2Temp;
         this.parkingLot = parkingLots[0];
-        this.parkingLot2 = parkingLots[1];
+        try{
+            parkingLot2Temp = parkingLots[1];
+        }catch (Exception e){
+            parkingLot2Temp = null;
+        }
+        this.parkingLot2 = parkingLot2Temp;
+        this.parkingLots.addAll(Arrays.asList(parkingLots));
     }
 
     public Ticket parking(Car car){
         boolean isParkingLot2Full = parkingLot2 == null || parkingLot2.isFull();
-        if (car == null || (parkingLot.isFull() && isParkingLot2Full) ){
+        if (car == null){
             return null;
         }
-        Ticket ticketResult;
-        if(parkingLot2 == null || parkingLot.getAvailablePositionRate()>parkingLot2.getAvailablePositionRate() || parkingLot.getCurrentSize() <= parkingLot2.getCurrentSize()){
-            ticketResult = parkingLot.parking(car);
-        }else {
-            ticketResult = parkingLot2.parking(car);
+        if ((parkingLot.isFull() && isParkingLot2Full)){
+            return null;
         }
+        boolean isAllParkingLotFull = true;
+        Ticket ticketResult = null;
+        for (ParkingLot parkingLot: parkingLots) {
+            if(!parkingLot.isFull()){
+                isAllParkingLotFull = false;
+                ticketResult = parkingLot.parking(car);
+                break;
+            }
+        }
+        if(isAllParkingLotFull){
+            // throw message: Not enough position.
+            return null;
+        }
+
         return ticketResult;
     }
 
@@ -55,5 +71,9 @@ public class ParkingBoy {
             return null;
         }
         return "Not enough position.";
+    }
+
+    public List<ParkingLot> getParkingLots(){
+        return parkingLots;
     }
 }
